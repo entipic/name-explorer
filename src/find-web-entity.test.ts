69c31f82c67f04024577075c1124104ac9bd029e
Dumitru C <canteadumitru@gmail.com>
2020-05-23 18:26:22 +0300

    format

--- src/find-web-entity.test.ts
+++ src/find-web-entity.test.ts
@@ -1,51 +1,52 @@
+import test from "ava";
+import { findWebEntity, getSimpleName } from "./find-web-entity";
+import { SimpleEntityType } from "wiki-entity";
 
-import test from 'ava';
-import { findWebEntity, getSimpleName } from './find-web-entity';
-import { SimpleEntityType } from 'wiki-entity';
+test("ro:SUA", async (t) => {
+  const entity = await findWebEntity("SUA", "ro");
+  if (!entity) {
+    return t.fail("Not found entity!");
+  }
+  t.is(entity.type, SimpleEntityType.PLACE);
+  t.is(entity.englishName, "United States");
+});
 
-test('ro:SUA', async t => {
-    const entity = await findWebEntity('SUA', 'ro');
-    if (!entity) {
-        return t.fail('Not found entity!');
-    }
-    t.is(entity.type, SimpleEntityType.PLACE);
-    t.is(entity.englishName, 'United States');
-})
+test("ro:adrian ursu", async (t) => {
+  const entity = await findWebEntity("adrian ursu", "ro");
+  if (!entity) {
+    return t.fail("Not found entity!");
+  }
+  t.is(entity.simpleName, "Adrian Ursu");
+});
 
-test('ro:adrian ursu', async t => {
-    const entity = await findWebEntity('adrian ursu', 'ro');
-    if (!entity) {
-        return t.fail('Not found entity!');
-    }
-    t.is(entity.simpleName, 'Adrian Ursu');
-})
+test("ro:md:adrian ursu", async (t) => {
+  const entity = await findWebEntity("adrian ursu", "ro", "md");
+  if (!entity) {
+    return t.fail("Not found entity!");
+  }
+  t.is(entity.name, "Adrian Ursu (cântăreț)");
+});
 
-test('ro:md:adrian ursu', async t => {
-    const entity = await findWebEntity('adrian ursu', 'ro', 'md');
-    if (!entity) {
-        return t.fail('Not found entity!');
-    }
-    t.is(entity.name, 'Adrian Ursu (cântăreț)');
-})
+test("ro:md:ministerul muncii", async (t) => {
+  const entity = await findWebEntity("ministerul muncii", "ro", "md");
+  if (!entity) {
+    return t.fail("Not found entity!");
+  }
+  t.true(
+    entity.wikiPageTitle && entity.wikiPageTitle.includes("Republica Moldova")
+  );
+});
 
-test('ro:md:ministerul muncii', async t => {
-    const entity = await findWebEntity('ministerul muncii', 'ro', 'md');
-    if (!entity) {
-        return t.fail('Not found entity!');
-    }
-    t.true(entity.wikiPageTitle && entity.wikiPageTitle.includes('Republica Moldova'));
-})
+test("ro-md:Chișinău", async (t) => {
+  const entity = await findWebEntity("Chișinău", "ro", "md");
+  if (!entity) {
+    return t.fail("Not found entity!");
+  }
+  // console.log(entity)
+  t.is(entity.type, SimpleEntityType.PLACE);
+  t.is(entity.englishName, "Chișinău");
+});
 
-test('ro-md:Chișinău', async t => {
-    const entity = await findWebEntity('Chișinău', 'ro', 'md');
-    if (!entity) {
-        return t.fail('Not found entity!');
-    }
-    // console.log(entity)
-    t.is(entity.type, SimpleEntityType.PLACE);
-    t.is(entity.englishName, 'Chișinău');
-})
-
-test('getSimpleName', t => {
-    t.is(getSimpleName('Name (aha)'), 'Name');
-})
+test("getSimpleName", (t) => {
+  t.is(getSimpleName("Name (aha)"), "Name");
+});
