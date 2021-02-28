ec8859dc785e0e622c52482ce745d8e36766d692
Dumitru C <canteadumitru@gmail.com>
2021-02-28 12:52:18 +0200

    update http headers

--- src/google-images.ts
+++ src/google-images.ts
@@ -29,15 +29,13 @@ export async function findImagesOnGoogle(
     url = url.replace("{country}", "");
   }
 
-  // console.log('image url', url);
-
   const reqOptions = {
     gzip: true,
     headers: {
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
       Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       "Accept-Language":
         "en-US,en;q=0.8,cs;q=0.6,es;q=0.4,hu;q=0.2,it;q=0.2,lt;q=0.2,ro;q=0.2,ru;q=0.2,sk;q=0.2,uk;q=0.2,pl;q=0.2,bg;q=0.2"
     }
